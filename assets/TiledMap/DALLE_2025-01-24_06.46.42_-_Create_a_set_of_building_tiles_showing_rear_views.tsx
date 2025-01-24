9e00f4ff1562764ae165666a3c673bd4dacd91d3
Axel Meunier <143808857+LeskGit@users.noreply.github.com>
2025-01-24 07:58:01 +0100

    v3

---
+++ assets/TiledMap/DALLE_2025-01-24_06.46.42_-_Create_a_set_of_building_tiles_showing_rear_views.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="DALLE_2025-01-24_06.46.42_-_Create_a_set_of_building_tiles_showing_rear_views" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../../../../Downloads/DALLE_2025-01-24_06.46.42_-_Create_a_set_of_building_tiles_showing_rear_views.png" width="1024" height="1024"/>
+</tileset>
---
+++ assets/TiledMap/image4-removebg-preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="image4-removebg-preview" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../../../../../../Downloads/image4-removebg-preview.png" width="106" height="131"/>
+</tileset>
