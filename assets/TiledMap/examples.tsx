fb78b3c3c64fb331b3e4a80ff42eb0b917d9bcbf
Axel Meunier <143808857+LeskGit@users.noreply.github.com>
2025-01-24 05:43:26 +0100

    v2

---
+++ assets/TiledMap/examples.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="examples" tilewidth="32" tileheight="32" tilecount="280" columns="20">
+ <image source="TiledSet/examples.png" width="655" height="459"/>
+</tileset>
