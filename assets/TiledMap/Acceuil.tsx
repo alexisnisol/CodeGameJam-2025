0fe32f3f63848bc38fbcac81b57ad427e3398696
moucmoi <noatoimoi@gmail.com>
2025-01-24 13:57:36 +0100

    réparation

--- assets/TiledMap/Acceuil.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="Acceuil" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="../Map_s_Psd_s/6nOD3a.png" width="960" height="960"/>
-</tileset>
